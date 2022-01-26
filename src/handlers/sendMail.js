55b6aafdc38ee8db1ccffb2a8f4b4c69182a60d8
Hiliyeh <hiliyeh.bashir@gmail.com>
2022-01-26 15:46:54 +0100

    email test

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,31 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "us-east-1" });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: "hiliyeh.bashir@gmail.com",
+    Destination: {
+      ToAddresses: ["hiliyeh.bashir@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello from Belgium !",
+        },
+      },
+      Subject: {
+        Data: "Test Mail",
+      },
+    },
+  };
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
