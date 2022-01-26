48b6858e203857c9181d6ebcdaf9a363e490a683
Hiliyeh <hiliyeh.bashir@gmail.com>
2022-01-26 16:15:51 +0100

    provisioning aws sqs

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,24 +1,31 @@
 import AWS from "aws-sdk";
 
-const ses = new AWS.SES({ region: "us-east-1" });
+const ses = new AWS.SES({ region: "eu-west-1" });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log("record processing", record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: "hiliyeh.bashir@gmail.com",
     Destination: {
-      ToAddresses: ["hiliyeh.bashir@gmail.com"],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: "Hello from Belgium !",
+          Data: body,
         },
       },
       Subject: {
-        Data: "Test Mail",
+        Data: subject,
       },
     },
   };
+
   try {
     const result = await ses.sendEmail(params).promise();
     console.log(result);
