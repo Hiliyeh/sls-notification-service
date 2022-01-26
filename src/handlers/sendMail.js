c0d76d6f48bcbbda8af416cf35a1a2842b6db40c
Hiliyeh <hiliyeh.bashir@gmail.com>
2022-01-26 17:58:26 +0100

    exporting variables cloudformation

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,6 @@
 import AWS from "aws-sdk";
 
-const ses = new AWS.SES({ region: "eu-west-1" });
+const ses = new AWS.SES({ region: "us-east-1" });
 
 async function sendMail(event, context) {
   const record = event.Records[0];
